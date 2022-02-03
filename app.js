8db35710b180c1162ce13fc2f09f44486da17376
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-02-02 21:56:44 -0300

    Token implementado

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { MongoClient } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import {v4 as uuid} from "uuid"
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ app.post("/cadastro", async (req, res) => {
   try {
     const userData = req.body;
     userData.password = bcrypt.hashSync(req.body.password, 10);
-    console.log(userData);
+    
     const mongoClient = new MongoClient(process.env.MONGO_URI);
     await mongoClient.connect();
 
@@ -40,9 +41,12 @@ app.post("/login", async (req, res) => {
 
     const dbAPIMyWallet = mongoClient.db("APIMyWallet");
     const usersCollection = dbAPIMyWallet.collection("usuarios");
+    const sessaoCollection = dbAPIMyWallet.collection("sessao");
     const user = await usersCollection.findOne({ email: req.body.email });
     if (user && bcrypt.compareSync(req.body.password, user.password)) {
-      res.send(user);
+      const token = uuid()
+      await sessaoCollection.insertOne({token: token, userId: user._id, email: user.email})
+      res.send({token:token, name:user.name});
     } else {
       res.sendStatus(401);
     }
@@ -56,10 +60,21 @@ app.post("/entrada", async (req, res) => {
     const inputData = req.body;
     const mongoClient = new MongoClient(process.env.MONGO_URI);
     await mongoClient.connect();
-
     const dbAPIMyWallet = mongoClient.db("APIMyWallet");
+
     const extratosCollection = dbAPIMyWallet.collection("extratos");
-    const extrato = await extratosCollection.insertOne({ ...inputData });
+    const sessaoCollection = dbAPIMyWallet.collection("sessao");
+
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+  
+    if(!token) return res.sendStatus(401);
+
+    const session = await sessaoCollection.findOne({ token });
+    if (!session) return res.sendStatus(401);
+
+    const extrato = await extratosCollection.insertOne({ ...inputData, email: session.email });
+    
     res.sendStatus(200);
   } catch {
     res.sendStatus(500);
@@ -71,10 +86,20 @@ app.post("/saida", async (req, res) => {
     const outputData = req.body;
     const mongoClient = new MongoClient(process.env.MONGO_URI);
     await mongoClient.connect();
-
     const dbAPIMyWallet = mongoClient.db("APIMyWallet");
+
     const extratosCollection = dbAPIMyWallet.collection("extratos");
-    const extrato = await extratosCollection.insertOne({ ...outputData });
+    const sessaoCollection = dbAPIMyWallet.collection("sessao");
+
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+  
+    if(!token) return res.sendStatus(401);
+
+    const session = await sessaoCollection.findOne({ token });
+    if (!session) return res.sendStatus(401);
+
+    const extrato = await extratosCollection.insertOne({ ...outputData, email: session.email });
     res.sendStatus(200);
   } catch {
     res.sendStatus(500);
@@ -85,10 +110,21 @@ app.get("/menu", async (req, res) => {
   try {
     const mongoClient = new MongoClient(process.env.MONGO_URI);
     await mongoClient.connect();
-
     const dbAPIMyWallet = mongoClient.db("APIMyWallet");
+    
+    const sessaoCollection = dbAPIMyWallet.collection("sessao");
     const extratosCollection = dbAPIMyWallet.collection("extratos");
-    const extrato = await extratosCollection.find({ email: req.headers.email }).toArray();
+
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+  
+    if(!token) return res.sendStatus(401);
+
+    const session = await sessaoCollection.findOne({ token });
+    if (!session) return res.sendStatus(401);
+
+
+    const extrato = await extratosCollection.find({ email: session.email }).toArray();
     res.send(extrato);
   } catch {
     res.sendStatus(500);
